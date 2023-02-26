f513b8fc2598f7e0d9ab1ff88fd6617611969c64
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-26 10:53:10 +0200

    Prepare the code for splitting into subcommands

--- src/serpent/analyse.py
+++ src/serpent/analyse.py
@@ -19,21 +19,23 @@ from serpent.stats import count_sorted
 from serpent.visual import interactive, plot_fft, plot_sequence_counts, show_image
 
 
-COUNT_LIMIT = 32
-PLOT = True
+COUNT_LIMIT = 20
+PLOT = False
 
 
 def analyse(data, fn=None):
 	data = dna.clean_non_dna(data)
 	data = pad_to_left(data, 3, "A")
 
+	# Codons
 	codons = dna.get_codons(data)
-	print("Codons:\n", codons)
-
 	counts = Counter(codons)
+	print("Codons used:\n", np.unique(codons))
+	print("Codons total:", len(codons), "unique:", len(np.unique(codons)))
 	print("Counts:")
 	pp(dict(counts.most_common()[:COUNT_LIMIT]))
 
+	# Decode
 	decoded = dna.decode(codons)
 	print("Decoded:\n", len(np.unique(decoded)), decoded)
 
@@ -43,55 +45,55 @@ def analyse(data, fn=None):
 		interactive()
 		plot_fft(decoded, n=64)
 		plot_sequence_counts(decoded, seq_length)
+		show_image(decoded, width=64, fill=63, mode="RGB")
+	else:
+		# Encode
+		encoded = "".join([alphabet64.get(c, " ") for c in decoded])
+		print("Encoded:\n", encoded)
+		counts = Counter(encoded)
+		print("Counts:")
+		pp(dict(counts.most_common()[:COUNT_LIMIT]))
 
-	encoded = "".join([alphabet64.get(c, " ") for c in decoded])
-	print("Encoded:\n", encoded)
+		# Write out base64 encoded data
+		if fn:
+			with open(fn + ".ser64", "w", encoding="UTF-8") as file:
+				file.write("".join(map_array(str, encoded)))
 
-	# Write out base64 encoded data
-	if fn:
-		with open(fn + ".ser64", "w", encoding="UTF-8") as file:
-			file.write("".join(map_array(str, encoded)))
+		# Bigrams:
+		ch_list = list(chunked(encoded, 2))
+		ch = map_array(lambda c: "".join(c), ch_list, dtype="U2")
+		counts = Counter(ch)
+		print("Bigrams:\n")
+		pp(dict(counts.most_common()[:COUNT_LIMIT]))
+		print("Bigrams not appearing:\n")
+		bigrams = combos[[cmb not in ch for cmb in combos]]
+		print(bigrams)
 
-	counts = Counter(encoded)
-	print("Counts:")
-	pp(dict(counts.most_common()[:COUNT_LIMIT]))
-
-	# Bigrams:
-	ch_list = list(chunked(encoded, 2))
-	ch = map_array(lambda c: "".join(c), ch_list, dtype="U2")
-	counts = Counter(ch)
-	print("Bigrams:\n")
-	pp(dict(counts.most_common()[:COUNT_LIMIT]))
-
-	print("Bigrams not appearing:\n")
-	bigrams = combos[[cmb not in ch for cmb in combos]]
-	print(bigrams)
-
-	# Print codon sequences
-	print("Codon sequences:\n")
-	seq_length = 4
-	[index, count] = count_sorted(dna.codon_sequences(decoded, seq_length))
-	twice_i = index[count == 2]
-	codes = map_array(
-		lambda a: pad_to_right(number_to_digits(a, 64), seq_length, 0), twice_i
-	)
-	b64_codes = map_array(
-		lambda a: "".join(list(map(alphabet64.get, number_to_digits(a)))), twice_i
-	)
-	print(b64_codes)
-
-	catg = map_array(
-		lambda a: "".join(
-			list(map(dna.bases_inverse.get, pad_to_right(number_to_digits(a, 4), 3, 0)))
-		),
-		codes.flatten(),
-	)
-
-	catg = catg.reshape(int(len(catg) / seq_length), seq_length)
-	catg = map_array(lambda row: "".join(row), catg)
-	print(catg)
-
-	show_image(decoded, width=64, fill=63, mode="RGB")
+		# Print codon sequences
+		print("Codon sequences:\n")
+		seq_length = 4
+		[index, count] = count_sorted(dna.codon_sequences(decoded, seq_length))
+		twice_i = index[count == 2]
+		codes = map_array(
+			lambda a: pad_to_right(number_to_digits(a, 64), seq_length, 0),
+			twice_i
+		)
+		b64_codes = map_array(
+			lambda a: "".join(list(map(
+				alphabet64.get,
+				number_to_digits(a)))),
+			twice_i
+		)
+		print(b64_codes)
+		catg = map_array(
+			lambda a: "".join(list(map(
+				dna.bases_inverse.get,
+				pad_to_right(number_to_digits(a, 4), 3, 0)))),
+			codes.flatten(),
+		)
+		catg = catg.reshape(int(len(catg) / seq_length), seq_length)
+		catg = map_array(lambda row: "".join(row), catg)
+		print(catg)
 
 	return decoded
 
