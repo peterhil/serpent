7021254f3a3eb637921339f6829c74d24980dc5f
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-02-27 05:05:43 +0200

    Clearer way of setting the bases

--- src/serpent/dna.py
+++ src/serpent/dna.py
@@ -11,9 +11,19 @@ from serpent.fun import map_array
 from serpent.padding import pad_to_left
 
 
-bases = {"A": 0b00, "C": 0b01, "G": 0b10, "T": 0b11, "U": 0b11}
-bases_inverse = {0: "A", 1: "C", 2: "G", 3: "T"}
-
+bases = dict(
+	A=0b00,
+	C=0b01,
+	G=0b10,
+	T=0b11,
+	U=0b11,
+)
+bases_inverse = {
+	0: "A",
+	1: "C",
+	2: "G",
+	3: "T"
+}
 
 def decode(dna):
 	"""Returns dna’s codons encoded into numbers 0..63"""
