c2b31b8fd1fc1a2f7544b1cff6d818a76528f6c8
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-03-04 00:56:57 +0200

    mypy: Fix Numpy array typing

--- src/serpent/mathematics.py
+++ src/serpent/mathematics.py
@@ -2,21 +2,23 @@
 
 from __future__ import annotations
 
-from collections.abc import Sequence
-from typing import Any, Union
+from typing import TypeVar, Union
 
 import numpy as np
+import numpy.typing as npt
 from numpy import dtype, ndarray
+
+T = TypeVar('T', bound=npt.NBitBase)
+
 
 LogBase = Union[int, float]
 Numeric = Union[int, float, complex]
-NpNumeric = ndarray[Numeric, dtype[Any]]
-NumericSeq = Union[Sequence[Numeric], NpNumeric]
+NumericSeq = npt.ArrayLike
 
 
 def logn(number: Numeric, base: LogBase=np.e) -> float:
 	"""Logarithm of number on some base."""
-	return np.log2(number) / np.log2(base)
+	return float(np.log2(number) / np.log2(base))
 
 
 def magnitude(data: NumericSeq, base: LogBase=64) -> int:
@@ -24,6 +26,6 @@ def magnitude(data: NumericSeq, base: LogBase=64) -> int:
 	return int(np.ceil(logn(np.max(data), base)))
 
 
-def normalise(seq: NumericSeq) -> NumericSeq:
+def normalise(seq: np.floating[T]) -> ndarray[T, dtype[np.floating[T]]]:
 	"""Normalise data max to be 1."""
 	return np.asanyarray(seq) / np.amax(seq)
