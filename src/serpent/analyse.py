d1a1dec11fa7bdb0f2b69a94cc2540831c3f7e82
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-03-11 08:11:19 +0200

    Keep the first value on autocorrelation plot

--- src/serpent/analyse.py
+++ src/serpent/analyse.py
@@ -58,7 +58,7 @@ def ac(filename, amino=False, limit=0.05, width=256, seq=1):
 	ac = autocorrelogram(decoded, width)
 
 	interactive()
-	plt.plot(np.arange(1, len(ac)), ac[1:], color=DEFAULT_COLOR)
+	plt.plot(ac, color=DEFAULT_COLOR)
 
 	peaks = ac_peaks(ac, limit)
 	{print("%s	%1.3f" % (peak, value)) for peak, value in peaks.items()}
