99e80bd2c0c4af25bca583a94689d210515f79a8
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-03-24 21:32:20 +0200

    More generic version of spread using more_itertools chunked and unzip

--- src/serpent/combinatorics.py
+++ src/serpent/combinatorics.py
@@ -2,25 +2,28 @@
 
 from __future__ import annotations
 
+from collections.abc import Sequence
+
+import more_itertools as mitr
 import numpy as np
 
 
-def spread(n: int=16, offset=0):
+def spread(seq: Sequence, n: int, *, offset: int=0):
 	"""Spread (or deal) a sequence evenly across N piles and combine them.
 
 	It's exactly like dealing a deck of cards across N players.
+	The offset parameter is like cutting the deck.
 
 	Example:
 	-------
-	>>> spread(16, offset=1)
+	>>> spread(np.arange(16), n=4, offset=1)
 	array([15,  0,  4,  8, 12,  1,  5,  9, 13,  2,  6, 10, 14,  3,  7, 11])
-	"""
-	seq = np.arange(n)
-	magn = int(np.ceil(np.log2(np.max(seq))))
-	max_n = 2 ** magn
 
-	offsetted = (seq - offset) % max_n
-	rounds = np.array(divmod((offsetted * magn), max_n))
-	spread = np.apply_along_axis(np.sum, 0, rounds)
+	>>> spread(np.arange(7), n=2, offset=0)
+	array([0, 2, 4, 6, 1, 3, 5])
+	"""
+	hands = mitr.chunked(seq, n)
+	shuffled = [list(hand) for hand in mitr.unzip(hands)]
+	spread = np.roll(np.concatenate(shuffled), offset)
 
 	return spread
