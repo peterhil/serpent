3b343f698ee5b445f13bac86d75190c77d734860
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-04-30 02:31:24 +0300

    Reuse start_and_stop on splitting functions

--- src/serpent/convert/split.py
+++ src/serpent/convert/split.py
@@ -3,17 +3,21 @@ from __future__ import annotations
 import more_itertools as mit
 
 
+def start_or_stop(a, b, start, stop, split='n'):
+	# 'sentences': Split when a start codon follows a stop codon
+	if split == 'n':
+		return (a in stop and b in start)
+	# 'words': Split on start or stop codons
+	elif split == 'r':
+		return (a in stop or b in start) and a != b
+
+
 def split_aminos(aminos, start='M', stop='*', split='r'):
 	"""Split amino acid sequence by start and stop codons."""
 	# TODO Get all start and stop codons from the genetic code tables!
-	def start_or_stop(a, b):
-		# 'sentences': Split when a start codon follows a stop codon
-		if split == 'n':
-			return (a in stop and b in start)
-		# 'words': Split on start or stop codons
-		elif split == 'r':
-			return (a in stop or b in start) and a != b
-	aminos = mit.split_when(aminos, start_or_stop)
+	def splitter(a, b):
+		return start_or_stop(a, b, start, stop, split)
+	aminos = mit.split_when(aminos, splitter)
 
 	return aminos
 
@@ -23,19 +27,14 @@ def split_nucleotides(
 	# Hard coded table 11, TODO Use genetic code tables
 	start='GTG ATG ATA ATC ATT CTG TTG',
 	stop='TGA TAG TAA',
-	split='n'):
-	"""Split nucleotide acid sequence by start and stop codons."""
-	# TODO Get all start and stop codons from the genetic code tables!
+	split='n'
+):
+	"""Split nucleotide sequence by start and stop codons."""
 	start = start.split(' ')
 	stop = stop.split(' ')
 
-	def start_or_stop(a, b):
-		# 'sentences': Split when a start codon follows a stop codon
-		if split == 'n':
-			return (a in stop and b in start)
-		# 'words': Split on start or stop codons
-		elif split == 'r':
-			return (a in stop or b in start) and a != b
+	def splitter(a, b):
+		return start_or_stop(a, b, start, stop, split)
+	codons = mit.split_when(codons, splitter)
 
-	codons = mit.split_when(codons, start_or_stop)
 	return codons
