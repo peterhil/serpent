676359d2da7ba5acab4ca6358fd6f45d725c586a
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-05-06 12:15:03 +0300

    Add two tests for is_degenerate

--- src/serpent/test/convert/degenerate_test.py
+++ src/serpent/test/convert/degenerate_test.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+import more_itertools as mit
 from numpy.testing import assert_array_equal
 
 from serpent import dna
+from serpent.convert.codon import codons_array
+from serpent.convert.degenerate import degenerate_codons, is_degenerate
+from serpent.fun import str_join
 
 
 def test_dna_decode_is_same_for_degenerate():
@@ -13,3 +17,16 @@ def test_dna_decode_is_same_for_degenerate():
 		dna.decode(data, degen=True) / scale,
 		dna.decode(data),
 	)
+
+
+def test_is_degenerate():
+	base_sets = [str_join(s) for s in mit.powerset('GACT')]
+	for nts in base_sets:
+		assert is_degenerate(nts) is False
+
+
+def test_is_degenerate_for_codons():
+	degens = degenerate_codons()
+	codons = codons_array()
+	for codon in degens:
+		assert is_degenerate(codon) == (codon not in codons)
