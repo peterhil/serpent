d229681da4ae9a57448762897594c096e1b13b7b
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-05-06 03:51:43 +0300

    mv: Rename analyse.py as cli/command.py

--- src/serpent/__init__.py
+++ src/serpent/__init__.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 from serpent import (
-         analyse,
+         cli,
          convert,
          dna,
          encoding,
--- src/serpent/__main__.py
+++ src/serpent/__main__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from serpent.analyse import main
+from serpent.cli.command import main
 
 if __name__ == "__main__":
 	main()
