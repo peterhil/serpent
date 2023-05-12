1a3d36edde560ef1735c9f1a3de7fd9f3315fe94
Peter Hillerström <peter.hillerstrom@gmail.com>
2023-05-13 00:33:19 +0300

    Refactor format_entropy

--- src/serpent/cli/entropy.py
+++ src/serpent/cli/entropy.py
@@ -5,17 +5,21 @@ import more_itertools as mit
 from serpent.math.information import statistics, statistics_header
 
 
-def format_entropy(data, base=2, seql=None, step=None):
-	if seql:
-		if step is None:
-			step = seql
-		if not 0 <= step <= seql:
-			err_msg = f'Step size should be between 0 and --seql/-q {seql} (inclusive)'
-			raise ValueError(err_msg)
+def check_step_size(seql, step):
+	if step is None:
+		step = seql
+	if not 0 <= step <= seql:
+		err_msg = f'Step size should be between 0 and --seql/-q {seql} (inclusive)'
+		raise ValueError(err_msg)
 
-		yield statistics_header()
+	return step
+
+
+def format_entropy(data, base=2, seql=None, step=None):
+	yield statistics_header()
+	if seql:
+		step = check_step_size(seql, step)
 		for chunk in mit.windowed(data, seql, step=step):
 			yield statistics(chunk, base)
 	else:
-		yield statistics_header()
 		yield statistics(data, base)
