a2a49ef1a2c419073de6c1051fc1ded3c7606043
Peter Hillerström <peter.hillerstrom@gmail.com>
2024-08-19 22:48:27 +0300

    test: Add simple tests for split_nucleotides

---
+++ src/serpent/test/convert/split_test.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from serpent.convert.split import split_nucleotides
+
+
+def test_split_nucleotides_n():
+	encoded = ['CCC', 'TAA', 'CCC', 'ATG', 'CCC', 'ATG', 'TAG', 'ATG', 'CCC', 'TAG']
+	expected = [
+		['CCC', 'TAA', 'CCC', 'ATG', 'CCC', 'ATG', 'TAG'],
+		['ATG', 'CCC', 'TAG']
+	]
+	split = 'n'
+	actual = list(split_nucleotides(encoded, table=11, split=split))
+
+	assert actual == expected
+
+
+def test_split_nucleotides_r():
+	encoded = ['CCC', 'TAA', 'CCC', 'ATG', 'CCC', 'ATG', 'TAG', 'ATG', 'CCC', 'TAG']
+	expected = [
+		['CCC', 'TAA'],
+		['CCC'],
+		['ATG', 'CCC'],
+		['ATG', 'TAG'],
+		['ATG', 'CCC', 'TAG']
+	]
+	split = 'r'
+	actual = list(split_nucleotides(encoded, table=11, split=split))
+
+	assert actual == expected
