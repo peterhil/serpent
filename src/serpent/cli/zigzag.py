b60f709d162d1cf0a1ef0dac57e1bff8295b0ee5
Peter Hillerström <peter.hillerstrom@gmail.com>
2024-09-04 21:43:36 +0300

    Clean up cli.zigzag module

--- src/serpent/cli/zigzag.py
+++ src/serpent/cli/zigzag.py
@@ -1,20 +1,15 @@
 from __future__ import annotations
 
-import colorsys
-import math
 from dataclasses import dataclass
 
 import blessed
-import numpy as np
 
 from serpent import dna
 from serpent.fun import str_join
 from serpent.io.fasta import auto_select_amino, descriptions_and_data, read_sequences
 from serpent.io.files import check_paths
-from serpent.math.basic import rescale
-from serpent.visual import ansi
 from serpent.visual.bitmap import decoded_to_pixels
-from serpent.visual.block_elements import HALF_BLOCK, pixels_to_blocks
+from serpent.visual.block_elements import pixels_to_blocks
 
 
 @dataclass
@@ -43,39 +38,6 @@ class ZigzagState:
 		self.file_no = (self.file_no - 1) % self.total
 		self.page_no = 0
 		self.dirty = True
-
-
-def rgb_at_xy(term, x, y, t):
-	h, w = term.height, term.width
-	xw = x - w / 2
-	yh = y - h / 2
-
-	hue = 4.0 + (
-		math.sin(x / 16)
-		+ math.sin(y / 32)
-		+ math.sin(math.sqrt(xw ** 2 + yh ** 2) / 8 + t * 3)
-	) + math.sin(math.sqrt(x ** 2 + y ** 2) / 8)
-	saturation = y / h
-	lightness = x / w
-
-	rgb = np.array(colorsys.hsv_to_rgb(hue / 8, saturation, lightness))
-	rgb = rescale(rgb, 256, 65536).astype(np.int32)
-
-	return rgb
-
-
-def screen_page(term, render_fn, t):
-	result = ''
-	for y in range(term.height - 1):
-		for x in range(term.width):
-			# fg = term.color_rgb(*render_fn(term, x, y, t))
-			# bg = term.on_color_rgb(*render_fn(term, x, y, t + 1))
-			# result += fg + bg + HALF_BLOCK
-			fg = render_fn(term, x, y, t)
-			bg = render_fn(term, x, y, t + 1)
-			colours = ansi.rgb(fg, bg)
-			result += colours + HALF_BLOCK
-	return result
 
 
 # ruff: noqa: PLR0913 # Too many arguments in function definition
